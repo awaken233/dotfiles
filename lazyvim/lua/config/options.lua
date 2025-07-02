b553b14bbb25f1412f0d488f0552acb20117e353
awaken233 <1083954992@qq.com>
2025-07-02 14:18:00 +0800

    在 options.lua 文件中添加代理环境变量配置，支持 HTTP 和 HTTPS 代理设置。

--- lazyvim/lua/config/options.lua
+++ lazyvim/lua/config/options.lua
@@ -5,3 +5,8 @@ local opt = vim.opt
 
 opt.clipboard = ""
 opt.expandtab = false -- Use spaces instead of tabs
+
+-- 配置代理环境变量
+vim.env.http_proxy = "http://127.0.0.1:7890"
+vim.env.https_proxy = "http://127.0.0.1:7890"
+vim.env.all_proxy = "socks5://127.0.0.1:7890"
